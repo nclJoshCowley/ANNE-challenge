ecc13f218879fa0ef88683eb2d0b86592f3eccf8
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-03-22 14:46:40 +0000

    Added helpful imports

---
+++ LAHS/R/utils-imports.R
@@ -0,0 +1,22 @@
+#' Imports
+#'
+#' Imports from other packages used within this package
+#'
+#' @name utils-imports
+#'
+#' @importFrom rlang .data
+#' @importFrom rlang %||%
+#' @importFrom magrittr %>%
+#'
+#' @keywords internal
+#'
+#' @section Links:
+#'   - \code{\link[rlang]{tidyeval-data}}, data pronoun.
+#'   - \code{\link[rlang]{op-null-default}}, default value for NULL operator.
+#'   - \code{\link[magrittr]{pipe}}, magrittr pipe (`%>%`)
+#'   - \code{\link[tidyselect]{where}}, tidyselect keyword.
+NULL
+
+
+#' @rdname utils-imports
+where <- utils::getFromNamespace("where", "tidyselect")
