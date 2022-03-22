40ab5d575af9f05b24a75127a7b0f165ff2a699c
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-03-22 19:18:36 +0000

    Documented and added dataset (in progress)

---
+++ LAHS/R/data.R
@@ -0,0 +1,14 @@
+#' Subset of Local Authority Housing Statistics
+#'
+#' We focus on the subset of the data that could be relevant from an
+#'   environmental perspective.
+#'
+#' @format A data frame with 3239 rows and ? variables:
+#' \describe{
+#'   \item{LA_CODE}{Local authority code at the time the data was collected}
+#'   \item{LA_NAME}{Local Authority name at the time the data was collected}
+#'   \item{LA_TYPE}{Current local authority type (April 2020)}
+#'   \item{LA_REGION}{English Region name}
+#'   \item{YEAR}{The starting year of the financial reporting year}
+#' }
+"LAHS"
