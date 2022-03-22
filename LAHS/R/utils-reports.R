5d777ebbcbbe449a28f8d1409e27e6a46034b023
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-03-22 16:10:29 +0000

    Created function to allow **selective** code hiding

--- LAHS/R/utils-reports.R
+++ LAHS/R/utils-reports.R
@@ -28,3 +28,28 @@ common_report_setup <- function(..., base_size = 16) {
 
   return(invisible(NULL))
 }
+
+
+#' Knitr Hook to Hide Source Code
+#'
+#' Creates a `source` hook to hide source code that could be of interest but
+#'   not pertinent to the report.
+#'
+#' @details This returns a function that should be set using `knit_hook$set()`.
+#'   That is, `knitr::knit_hooks$set(source = get_knit_hook_source_hide())`.
+#'
+#' @export
+get_knit_hook_source_hide <- function() {
+  original_hook <- knitr::knit_hooks$get("source")
+
+  function(x, options) {
+    original_source_output <- original_hook(x, options)
+    if (is.null(options$hide)) return(original_source_output)
+
+    sprintf(
+      "<details><summary>%s</summary>\n\n%s\n\n</details>",
+      options$hide,
+      original_source_output
+    )
+  }
+}
