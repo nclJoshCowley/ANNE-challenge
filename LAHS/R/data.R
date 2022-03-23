76fbf9a49f24d75b894bb0f54835c9641a01ca12
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-03-23 21:04:00 +0000

    First draft of cleaned dataset. LAHS
    Also set report set to not overwrite data and compiled HTML

--- LAHS/R/data.R
+++ LAHS/R/data.R
@@ -3,7 +3,7 @@
 #' We focus on the subset of the data that could be relevant from an
 #'   environmental perspective.
 #'
-#' @format A data frame with 3239 rows and ? variables:
+#' @format A data frame with 3239 rows and 23 variables:
 #' \describe{
 #'   \item{LA_CODE}{Local authority code at the time the data was collected.}
 #'   \item{LA_NAME}{Local Authority name at the time the data was collected.}
@@ -16,6 +16,20 @@
 #'   \item{STOCK_VALUE}{Value of dwellings owned by encircling LA in millions
 #'       of pounds to 3.d.p.}
 #'   \item{EPC}{EPC Band of the average SAP rating.}
+#'   \item{NEW_BOILERS}{Boiler replacements, regardless of the energy
+#'       efficiency rating of the boiler.}
+#'   \item{INSULATION}{Total number of insulations installed.}
+#'   \item{INSULATION_SOLID}{Number of solid wall insulations.}
+#'   \item{INSULATION_CAVITY}{Number of cavity wall insulations.}
+#'   \item{INSULATION_LOFT}{Number of loft or roof insulations.}
+#'   \item{INSULATION_FLOOR}{Number of floor insulations.}
+#'   \item{RT_TOTAL}{Number of dwellings with renewable technology installed.}
+#'   \item{RT_PVPANELS}{True if any photovoltaic panels have been installed.}
+#'   \item{RT_SOLARTHERMAL}{True if any solar thermals have been installed.}
+#'   \item{RT_HEATPUMP}{True if any heat pumps have been installed.}
+#'   \item{RT_BIOMASS}{True if any biomass boilers have been installed.}
+#'   \item{RT_TURBINE}{True if any wind turbines have been installed.}
+#'   \item{RT_OTHER}{True if any other technologies have been installed.}
 #' }
 #'
 #' @source <https://www.gov.uk/government/statistical-data-sets/local-authority-housing-statistics-open-data>
