571b5abc038cd9d22a7420e6e5b222b43dc6fb1e
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-03-23 17:44:57 +0000

    Updated documentation to match latest dataset

--- LAHS/R/data.R
+++ LAHS/R/data.R
@@ -5,11 +5,17 @@
 #'
 #' @format A data frame with 3239 rows and ? variables:
 #' \describe{
-#'   \item{LA_CODE}{Local authority code at the time the data was collected}
-#'   \item{LA_NAME}{Local Authority name at the time the data was collected}
-#'   \item{LA_TYPE}{Current local authority type (April 2020)}
-#'   \item{LA_REGION}{English Region name}
-#'   \item{YEAR}{The starting year of the financial reporting year}
+#'   \item{LA_CODE}{Local authority code at the time the data was collected.}
+#'   \item{LA_NAME}{Local Authority name at the time the data was collected.}
+#'   \item{LA_TYPE}{Current local authority type (April 2020).}
+#'   \item{LA_REGION}{English Region name.}
+#'   \item{YEAR}{The starting year of the financial reporting year.}
+#'   \item{STOCK_PUBLIC_LA}{Number of dwellings owned by **any** LA.}
+#'   \item{STOCK_PUBLIC_OTHER}{Other public sector owned dwellings.}
+#'   \item{STOCK}{Number of dwellings owned by encircling LA.}
+#'   \item{STOCK_VALUE}{Value of dwellings owned by encircling LA in millions
+#'       of pounds to 3.d.p.}
+#'   \item{EPC}{EPC Band of the average SAP rating.}
 #' }
 #'
 #' @source <https://www.gov.uk/government/statistical-data-sets/local-authority-housing-statistics-open-data>
