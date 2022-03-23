763f6eafd27120676f8ce6e44ef97aa31aaff00d
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-03-23 20:53:12 +0000

    Added renewables section

--- LAHS/data-raw/LAHS-data-cleaning-helper.R
+++ LAHS/data-raw/LAHS-data-cleaning-helper.R
@@ -21,3 +21,36 @@ parse_la_type <- function(x) {
   return(out)
 }
 
+
+#' Parse Renewable Technologies Coding
+#'
+#' In LAHS data, the renewable technology types are coded as integers (0-63),
+#'   that when converted to binary will denote 6 binary variables.
+#'
+#' @param x integer vector. Vector of values from column f2daa.
+#'
+#' @return data frame with `length(x)` rows and 6 named columns.
+parse_rt_types <- function(x) {
+  stopifnot(is.numeric(x))
+
+  if (anyNA(x)) stop("Missing values: ", toString(which(is.na(x))))
+
+  is_invalid <- (x >= 64 | x < 0)
+  if (any(is_invalid)) stop("Invalid values: ", toString(x[is_invalid]))
+
+  rt_names <-
+    c(
+      "RT_PVPANELS",
+      "RT_SOLARTHERMAL",
+      "RT_HEATPUMP",
+      "RT_BIOMASS",
+      "RT_TURBINE",
+      "RT_OTHER"
+    )
+
+  purrr::map_dfr(x, function(.x) {
+    # Get the 6 Least-Significant-Bits as TRUE / FALSE
+    vec <- as.logical(intToBits(.x)[1:6])
+    structure(as.list(vec), .Names = rt_names)
+  })
+}
