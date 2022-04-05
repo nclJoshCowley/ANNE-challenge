c00184eefd5f272713f9972210f98330a1c13bda
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-04-05 16:52:18 +0100

    Made "Unknown" house type explicit (instead of NA)

--- LAHS/R/condense.R
+++ LAHS/R/condense.R
@@ -11,22 +11,22 @@ NULL
 #' @describeIn condense Convert dwelling type from type/age to just type.
 #' @export
 condense_alltypex <- function(x) {
-  forcats::fct_collapse(
-    x,
-    Flat = c(
-      "Purpose built flat, high rise",
-      "Purpose built flat, low rise",
-      "Converted flat"
-    ),
-    House = c(
-      "Detached house : pre 1919",
-      "Detached house : post 1919",
-      "Semi detached & terraced : pre 1919",
-      "Semi detached & terraced : 1919-1944",
-      "Semi detached & terraced : 1945-1964",
-      "Semi detached & terraced : 1965 onwards"
-    ),
-    Bungalow = "Bungalow : all ages",
-    Unknown = "NA"
-  )
+  x %>%
+    forcats::fct_collapse(
+      Flat = c(
+        "Purpose built flat, high rise",
+        "Purpose built flat, low rise",
+        "Converted flat"
+      ),
+      House = c(
+        "Detached house : pre 1919",
+        "Detached house : post 1919",
+        "Semi detached & terraced : pre 1919",
+        "Semi detached & terraced : 1919-1944",
+        "Semi detached & terraced : 1945-1964",
+        "Semi detached & terraced : 1965 onwards"
+      ),
+      Bungalow = "Bungalow : all ages"
+    ) %>%
+    forcats::fct_explicit_na("Unknown")
 }
